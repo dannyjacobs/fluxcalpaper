1cb66006e3431e81ce526e6cfac10462ce85dd6e
Danny Jacobs <daniel.c.jacobs@asu.edu>
2013-05-01 10:27:21 -0700

    adding calculation of simulated dec dep gain, code cleanup, makes images with labels

--- plots/beam_cal_sim.py
+++ plots/beam_cal_sim.py
@@ -26,7 +26,7 @@ dt = 10 /(24*60.)
 srclist,cutoff,catalogs = a.scripting.parse_srcs(opts.src, opts.cat)
 cat = a.cal.get_catalog(opts.cal, srclist, cutoff, catalogs)
 assert(len(cat)>0)#make sure the catalog got found
-if len(args)>0
+if len(args)>0:
     print "loading beam files ",
     print args
     files = args
@@ -54,6 +54,14 @@ if len(args)>0
         phi = azalt[0]*180/n.pi #phi is longitude
         if opts.pol.startswith('y'): phi -= 90
         return BEAM[:,floor(phi),floor(theta)]
+    def rotbradleybeam(src):
+        azalt = src.get_crds('top',ncrd=2)
+        theta = (n.pi/2 - azalt[1])*180/n.pi #theta is the polar coord, zero at pole
+        phi = azalt[0]*180/n.pi #phi is longitude
+        if opts.pol.startswith('y'): phi -= 90
+        phi -= 90
+        return BEAM[:,floor(phi),floor(theta)]
+        
 def rotbeam(src):
     xyz = src.get_crds('top')
     if opts.pol.startswith('x'):pol = 'y'
@@ -62,8 +70,13 @@ def rotbeam(src):
 def calbeam(src):
     xyz = src.get_crds('top')
     return aa[0].bm_response(xyz,pol=opts.pol[0]).squeeze()**2
-
-plotfreq = n.median(range(len(freqs))).astype(int)
+def ModelBeam(src):
+    return bradleybeam(src)
+def TrueBeam(src):
+    return rotbradleybeam(src)
+#    return calbeam(src)
+nfreqs = len(freqs)
+plotfreq = int(nfreqs/2)
 print "loading cal file"
 aa = a.cal.get_aa(opts.cal,freqs)
 cat.compute(aa)
@@ -73,46 +86,145 @@ subplot(211)
 pos = {}
 Abeam = {}
 Bbeam = {}
-figure(1)
+
+print "Beam model amplitudes at meridian"
 for src in cat:
-    pos[src] = []
-    Abeam[src] = []
-    Bbeam[src] = []
-    aa.date = aa.next_rising(cat[src])
+    aa.date = aa.next_transit(cat[src])
+    cat.compute(aa)
+    print "beamfreq=",aa.get_afreqs()[3]
+    print src,cat[src].get_crds('top',ncrd=2),
+    print ModelBeam(cat[src])[3],TrueBeam(cat[src])[3]
+if False:
+    figure(1)
+    for src in cat:
+        pos[src] = []
+        Abeam[src] = []
+        Bbeam[src] = []
+        times = []
+        risetime = aa.next_rising(cat[src])
+        aa.date = risetime
+        print src
+        while(True):
+            t = aa.date
+            aa.set_ephemtime(t+dt)
+            cat.compute(aa)
+            src_xyz = cat[src].get_crds('top')
+            if src_xyz[2]<0:break
+            times.append(t-risetime)
+            src_angle = cat[src].get_crds('top',ncrd=2)
+            #print src_angle,bradleybeam(cat[src])[plotfreq],calbeam(cat[src])[plotfreq]
+            pos[src].append(src_angle)
+    #        Abeam[src].append(calbeam(cat[src]))
+    #        Bbeam[src].append(rotbeam(cat[src]))    
+            Abeam[src].append(bradleybeam(cat[src]))
+            Bbeam[src].append(rotbradleybeam(cat[src]))
+    
+    
+        pos[src] = n.array(pos[src])
+        Abeam[src] = n.array(Abeam[src])
+        Bbeam[src] = n.array(Bbeam[src])
+        times = n.array(times)*24 #time in hours
+        if src=='zen': color = '0.5'
+        else: color = 'k'
+        HA = (pos[src][:,0]-n.pi)*12/n.pi
+        peaktime = times[pos[src][:,1].argmax()]
+        plot(times-peaktime,Abeam[src][:,plotfreq],color,label=src)
+        plot(times-peaktime,Bbeam[src][:,plotfreq],color,linestyle=':',label=src)
+    xlabel('time [h]')
+    ylabel('beam amplitude')
+    grid()
+    subplot(212)
+    for src in cat:
+        print Abeam[src].shape,Bbeam[src].shape
+        bandpass = n.sum(Abeam[src] * Bbeam[src],axis=0)/n.sum(Bbeam[src]**2,axis=0)
+        if not opts.calsrc is None:
+            bandpass /= (n.sum(Abeam[opts.calsrc] * Bbeam[opts.calsrc],axis=0)/n.sum(Bbeam[opts.calsrc]**2,axis=0))
+        if src=='zen': color='0.5'
+        else: color='k'
+        plot(freqs*1e3,bandpass,color,label=src)
+    xlabel('freqs [MHz]')
+    ylabel('passband amplitude')
+    grid()
+
+
+#scan through a range of decs and simulate the weight factor g using two different beams
+decs = n.arange(-59,-30,1)
+srclist = ','.join(['0_%d'%dec for dec in decs])
+srcs,coff,catalogs = a.scripting.parse_srcs(srclist,'')
+cat = a.src.get_catalog(srcs,catalogs)
+cat.compute(aa)
+g = n.zeros((nfreqs,len(cat)))
+w = n.zeros((nfreqs,len(cat)))
+A = {}
+B = {}
+W = {}
+crds = {}
+times = {}
+for i,src in enumerate(n.sort(cat.keys())[::-1]):
     print src
-    while(True):
+    aa.date = aa.next_rising(cat[src])
+    A[src] = []
+    B[src] = []
+    crds[src] = []
+    W[src] = n.zeros(nfreqs)
+    times[src] = []
+    while True:
         t = aa.date
         aa.set_ephemtime(t+dt)
-        cat.compute(aa)
+        cat.compute(aa)        
         src_xyz = cat[src].get_crds('top')
         if src_xyz[2]<0:break
-        src_angle = cat[src].get_crds('top',ncrd=2)
-        print src_angle,bradleybeam(cat[src])[plotfreq],calbeam(cat[src])[plotfreq]
-        pos[src].append(src_angle)
-        Abeam[src].append(calbeam(cat[src]))
-#        Bbeam[src].append(bradleybeam(cat[src]))
-        Bbeam[src].append(rotbeam(cat[src]))
-    pos[src] = n.array(pos[src])
-    Abeam[src] = n.array(Abeam[src])
-    Bbeam[src] = n.array(Bbeam[src])
-    if src=='zen': color = '0.5'
-    else: color = 'k'
-    plot((pos[src][:,0]-n.pi)*12/n.pi,Abeam[src][:,plotfreq],color,label=src)
-    plot((pos[src][:,0]-n.pi)*12/n.pi,Bbeam[src][:,plotfreq],color,linestyle=':',label=src)
-xlabel('HA')
-ylabel('beam amplitude')
-grid()
-subplot(212)
-for src in cat:
-    print Abeam[src].shape,Bbeam[src].shape
-    bandpass = n.sum(Abeam[src] * Bbeam[src],axis=0)/n.sum(Bbeam[src]**2,axis=0)
-    if not opts.calsrc is None:
-        bandpass /= (n.sum(Abeam[opts.calsrc] * Bbeam[opts.calsrc],axis=0)/n.sum(Bbeam[opts.calsrc]**2,axis=0))
-    if src=='zen': color='0.5'
-    else: color='k'
-    plot(freqs*1e3,bandpass,color,label=src)
-xlabel('freqs [MHz]')
-ylabel('passband amplitude')
-grid()
+        #print src_xyz
+        crds[src].append(src_xyz)
+        g[:,i] += ModelBeam(cat[src]) * TrueBeam(cat[src])
+        w[:,i] += ModelBeam(cat[src]) * ModelBeam(cat[src])
+        A[src].append(ModelBeam(cat[src]))
+        B[src].append(TrueBeam(cat[src]))
+        W[src] += ModelBeam(cat[src]) * ModelBeam(cat[src])
+        times[src].append(t)
 
-show()
+g /= w
+#TODO add a pane to the weighted plot showing the un-weighted tracks
+figure()
+clf()
+print n.max(A['0_-40']),n.argmax(A['0_-40']),len(crds['0_-40'])#crds['0_-40'][n.argmax(A['0_-40'])]
+print n.max(A['0_-50']),n.argmax(A['0_-50']),len(crds['0_-50'])##crds['0_-50'][n.argmax(A['0_-50'])]
+
+for src in ['0_-40','0_-50']:
+    t = n.array(times[src])
+    t -= n.median(t)
+    t *= 24
+    B_A = n.array(A[src]).copy()
+    B_M = n.array(B[src]).copy()
+    subplot(211)
+    plot(t,B_A[:,plotfreq])
+    plot(t,B_M[:,plotfreq],ls=':')
+    subplot(212)
+    print src,B_A[:,plotfreq].max(),B_M[:,plotfreq].max(),(B_A[:,plotfreq]*B_M[:,plotfreq]).max()
+    plot(t,B_A[:,plotfreq]*B_M[:,plotfreq])
+#    G = n.array([q/sw[src] for q in sg[src]])
+#    plot((t-n.median(t))*24,G[:,plotfreq],'k')
+subplots_adjust(bottom=0.1,left=0.1)
+xlabel('time [hrs]')
+ylabel('$B_M(\\theta,\\phi) B_A(\\theta,\\phi)$')
+print "Model weighted, simulated beam"
+savefig('model_weighted_beam_sim_track.png')
+print "\t model_weighted_beam_sim_track.png"
+figure()
+subplot(211)
+for i in range(len(decs)):
+    plot(freqs*1e3,g[:,i],'0.5')
+grid()
+xlabel('freq [MHz]')
+ylabel('g($\\delta$)')
+subplot(212)
+plot(decs,g[plotfreq,:],'k')
+subplots_adjust(bottom=0.1,left=0.1)
+xlabel('$\\delta$  [deg]')
+ylabel('g($\\delta$)')
+grid()
+savefig('beam_gain_sim.png')
+print "Model-weighted beam integration sim. passband and declination dependence"
+print "\t beam_gain_sim.png"
+
+#show()
